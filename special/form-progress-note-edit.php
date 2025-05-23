cf694b59743d90b13ed42f0f15f906283e5561cb
khomson7 <khomson7@gmail.com>
2025-05-23 14:25:10 +0700

    update

--- special/form-progress-note-edit.php
+++ special/form-progress-note-edit.php
@@ -164,7 +164,8 @@ if ($row  = $stmt->fetch()) {
                             <div class="col-md-12 text-right">
                             <?php
                                     if((
-                                        Session::checkPermission('PPRS_PROGRESS_NOTE_EDIT','EDIT')
+                                        
+                                        Session::checkPermission('PRS_PROGRESS_NOTE_EDIT','EDIT')
                                     ) && (ReportQueryUtils::checkReadOnly($an))) { ?>
                                     <button type="button" class="btn btn-primary" id="btn_progress_note" onclick="form_save()"><i class="fas fa-save"></i> บันทึก</button>
                                 <?php } ?>
